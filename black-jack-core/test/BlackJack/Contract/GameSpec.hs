8379af475b86345f5c775762f7193cd3e3366027
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-10-09 19:03:59 +0200

    Add JSON roundtrip test

---
+++ black-jack-core/test/BlackJack/Contract/GameSpec.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TypeApplications #-}
+
+module BlackJack.Contract.GameSpec where
+
+import BlackJack.Contract.Game (Color, Card, DealerHand)
+import BlackJack.Game ()
+import Test.Aeson.GenericSpecs (roundtripAndGoldenSpecs, Proxy (Proxy))
+import Test.Hspec (Spec)
+
+spec :: Spec
+spec = do
+  roundtripAndGoldenSpecs (Proxy @Color)
+  roundtripAndGoldenSpecs (Proxy @Card)
+  roundtripAndGoldenSpecs (Proxy @DealerHand)
